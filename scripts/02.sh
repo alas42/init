045b99bc17fe77158034e002604f8480ace20ee0
Alexandre VOGT <avogt@e2r1p12.42.fr>
2019-02-22 18:18:39 +0100

    manque plus que le script 3

---
+++ scripts/02.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+	echo "Enter something please"
+	exit 1
+else
+	if [ "$(getent passwd $1)" ]
+	then
+		uid= `id -u $1`
+		pkill -U $1
+		userdel -r -f "$1"
+	else
+		echo "Please, enter a valid username, $1 does not exist."
+	fi	
+fi

134d9c5683f345d7d670af4ef4e5c70765f3b2aa
Alexandre VOGT <avogt@e1r12p20.42.fr>
2019-09-17 14:36:52 +0200

    init

--- scripts/02.sh
+++ scripts/02.sh
@@ -5,7 +5,8 @@ then
 	echo "Enter something please"
 	exit 1
 else
-	if [ "$(getent passwd $1)" ] && [ $1 != "nobody" ]; then
+	if [ "$(getent passwd $1)" ]
+	then
 		uid= `id -u $1`
 		pkill -U $1
 		userdel -r -f "$1"
